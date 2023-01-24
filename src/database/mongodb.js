0347688a31b32da07993eebca910af61923f7fc3
PopescuSabinTheodor <popescusabintheodor@gmail.com>
2023-01-25 00:46:53 +0200

    Move files

---
+++ src/database/mongodb.js
@@ -0,0 +1,16 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri =
+  "mongodb+srv://admin:admin@memento.djbtvpg.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
